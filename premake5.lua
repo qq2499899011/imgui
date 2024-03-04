672e748c15f7ff3a38a0b6b6e6f0462f664ebb97
chenyue <2499899011@qq.com>
2024-03-04 11:22:41 +0800

    premake and imgui.cpp

--- premake5.lua
+++ premake5.lua
@@ -1,38 +1,32 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
-    staticruntime "off"
-
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
-
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_tables.cpp",
+        "imgui_demo.cpp"
+    }
+    
 	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
+        systemversion "latest"
+        cppdialect "C++17"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:Debug"
+	    runtime "Debug"
+	    symbols "on"
 
 	filter "configurations:Release"
 		runtime "Release"
@@ -42,3 +36,6 @@ project "ImGui"
 		runtime "Release"
 		optimize "on"
         symbols "off"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
